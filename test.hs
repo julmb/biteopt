f3f1b1539e88d31fe81fb83c60b333649675e6fe
Julian Brunner <julian.brunner@gmail.com>
2022-04-29 16:58:27 +0200

    cleanup

--- test.hs
+++ test.hs
@@ -1,23 +1,9 @@
 module Main where
 
 import Numeric.Biteopt
-import Foreign.C
-import Foreign
 
-test :: Num a => [a] -> a
-test xy = (a - x) ^ 2 + b * (y - x ^ 2) ^ 2 where [x, y] = xy; a = 1; b = 100
-
-test' :: Objective
-test' n x d = do
-    xs <- peekArray (fromIntegral n) x
-    return $ test xs
-
-lb :: [CDouble]
-lb = [-2, -2]
-ub :: [CDouble]
-ub = [2, 2]
+rosenbrock :: Num a => [a] -> a
+rosenbrock xy = (a - x) ^ 2 + b * (y - x ^ 2) ^ 2 where [x, y] = xy; a = 1; b = 100
 
 main :: IO ()
-main = do
-    result <- minimize [(-2, 2), (-2, 2)] test
-    print result
+main = minimize [(-2, 2), (-2, 2)] rosenbrock >>= print
