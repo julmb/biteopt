026a96500055d4d1f07c83fe55a9f303906501ab
Julian Brunner <julian.brunner@gmail.com>
2022-04-29 16:23:30 +0200

    working rosenbrock test

--- source/test.hs
+++ source/test.hs
@@ -14,15 +14,31 @@ foreign import ccall "minimize" minimize ::
     CInt -> FunPtr RNG -> Ptr () -> IO CInt
 
 test :: Num a => [a] -> a
-test xy = (x - 1) ^ 2 + 2 * (y + 1) ^ 2 where [x, y] = xy
+test xy = (a - x) ^ 2 + b * (y - x ^ 2) ^ 2 where [x, y] = xy; a = 1; b = 100
 
 test' :: Objective
-test' n x d = return 0
+test' n x d = do
+    xs <- peekArray (fromIntegral n) x
+    return $ test xs
+
+lb :: [CDouble]
+lb = [-2, -2]
+ub :: [CDouble]
+ub = [2, 2]
 
 main :: IO ()
 main = do
     putStrLn "test"
     objective <- mkObjective test'
-
+    let n = 2
+    allocaArray n $ \ x ->
+        alloca $ \ fx ->
+            withArray lb $ \ lba ->
+                withArray ub $ \ uba -> do
+                    c <- minimize (fromIntegral n) objective nullPtr lba uba x fx 20000 1 1 1 nullFunPtr nullPtr
+                    print c
+                    xs <- peekArray n x
+                    print xs
+                    a <- peek fx
+                    print a
     freeHaskellFunPtr objective
-
