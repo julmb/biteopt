0c80a381317e56db0b739d034ff4c5cb8d0e2d72
Julian Brunner <julian.brunner@gmail.com>
2022-04-29 16:51:13 +0200

    continue working on library

--- source/Numeric/Biteopt.hs
+++ source/Numeric/Biteopt.hs
@@ -2,13 +2,34 @@ module Numeric.Biteopt where
 
 import Foreign
 import Foreign.C
+import Data.Coerce
 
 type Objective = CInt -> Ptr CDouble -> Ptr () -> IO CDouble
 type RNG = Ptr () -> IO CUInt
 
 foreign import ccall "wrapper" mkObjective :: Objective -> IO (FunPtr Objective)
-foreign import ccall "minimize" minimize ::
+foreign import ccall "minimize" boMinimize ::
     CInt -> FunPtr Objective -> Ptr () ->
     Ptr CDouble -> Ptr CDouble -> Ptr CDouble -> Ptr CDouble ->
     CInt -> CInt -> CInt ->
     CInt -> FunPtr RNG -> Ptr () -> IO CInt
+
+oo :: ([Double] -> Double) -> Objective
+oo objective n x d = do
+    xs <- peekArray (fromIntegral n) x
+    return $ coerce $ objective $ coerce xs
+
+minimize :: Int -> [(Double, Double)] -> ([Double] -> Double) -> IO ([Double], Double, CInt)
+minimize dimensions bounds objective = do
+    obj <- mkObjective $ oo objective
+    let (lbl, ubl) = unzip $ coerce bounds
+    (c, x, fx) <- allocaArray dimensions $ \ x ->
+        alloca $ \ fx ->
+            withArray lbl $ \ lba ->
+                withArray ubl $ \ uba -> do
+                    c <- boMinimize (fromIntegral dimensions) obj nullPtr lba uba x fx 20000 1 1 1 nullFunPtr nullPtr
+                    xs <- peekArray dimensions x
+                    a <- peek fx
+                    return (c, xs, a)
+    freeHaskellFunPtr obj
+    return (coerce x, coerce fx, c)
--- test.hs
+++ test.hs
@@ -19,17 +19,5 @@ ub = [2, 2]
 
 main :: IO ()
 main = do
-    putStrLn "test"
-    objective <- mkObjective test'
-    let n = 2
-    allocaArray n $ \ x ->
-        alloca $ \ fx ->
-            withArray lb $ \ lba ->
-                withArray ub $ \ uba -> do
-                    c <- minimize (fromIntegral n) objective nullPtr lba uba x fx 20000 1 1 1 nullFunPtr nullPtr
-                    print c
-                    xs <- peekArray n x
-                    print xs
-                    a <- peek fx
-                    print a
-    freeHaskellFunPtr objective
+    result <- minimize 2 [(-2, 2), (-2, 2)] test
+    print result
