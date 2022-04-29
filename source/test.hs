5a3bab3fe9fa31a6d9346451b24a7d053509e150
Julian Brunner <julian.brunner@gmail.com>
2022-04-29 15:59:07 +0200

    start working on ffi

--- source/test.hs
+++ source/test.hs
@@ -1,4 +1,28 @@
 module Main where
 
+import Foreign
+import Foreign.C
+
+type Objective = CInt -> Ptr CDouble -> Ptr () -> IO CDouble
+type RNG = Ptr () -> IO CUInt
+
+foreign import ccall "wrapper" mkObjective :: Objective -> IO (FunPtr Objective)
+foreign import ccall "minimize" minimize ::
+    CInt -> FunPtr Objective -> Ptr () ->
+    Ptr CDouble -> Ptr CDouble -> Ptr CDouble -> Ptr CDouble ->
+    CInt -> CInt -> CInt ->
+    CInt -> FunPtr RNG -> Ptr () -> IO CInt
+
+test :: Num a => [a] -> a
+test xy = (x - 1) ^ 2 + 2 * (y + 1) ^ 2 where [x, y] = xy
+
+test' :: Objective
+test' n x d = return 0
+
 main :: IO ()
-main = putStrLn "test"
+main = do
+    putStrLn "test"
+    objective <- mkObjective test'
+
+    freeHaskellFunPtr objective
+
