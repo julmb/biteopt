790ed12dfb0793ca2af4f3adb1f55b66840be039
Julian Brunner <julian.brunner@gmail.com>
2022-04-30 04:25:32 +0200

    go back to flat wrapper

--- biteopt.cxx
+++ biteopt.cxx
@@ -1,40 +1,28 @@
 #include "biteopt/biteopt.h"
 
-// TODO: public inheritance?
 // TODO: const modifiers?
-class Minimizer : CBiteOptMinimize
+
+extern "C" CBiteRnd* rnd_new() { return new CBiteRnd(); }
+extern "C" void rnd_free(CBiteRnd* rnd) { delete rnd; }
+extern "C" void rnd_init(CBiteRnd* rnd, int seed, biteopt_rng rf, void* rdata) { rnd->init(0, rf, rdata); }
+
+extern "C" CBiteOptMinimize* opt_new() { return new CBiteOptMinimize(); }
+extern "C" void opt_free(CBiteOptMinimize* opt) { delete opt; }
+extern "C" void opt_set(CBiteOptMinimize* opt, int N, biteopt_func f, void* data, double* lb, double* ub)
 {
-	CBiteRnd rnd;
-
-	public: Minimizer(int N, biteopt_func f, void* data, double* lb, double* ub, int M, biteopt_rng rf)
-	{
-		this->N = N;
-		this->f = f;
-		this->data = data;
-		this->lb = lb;
-		this->ub = ub;
-		
-		updateDims(N, M);
-
-		rnd.init(0, rf);
-
-		init(rnd);
-	}
-
-	public: int step() { return optimize(rnd); }
-	public: void best(double* x) { memcpy(x, getBestParams(), N * sizeof(double)); }
-};
+	opt->N = N;
+	opt->f = f;
+	opt->data = data;
+	opt->lb = lb;
+	opt->ub = ub;
+}
+extern "C" void opt_dims(CBiteOptMinimize* opt, int N, int M) { opt->updateDims(N, M); }
+extern "C" void opt_init(CBiteOptMinimize* opt, CBiteRnd* rnd) { opt->init(*rnd); }
+extern "C" int opt_step(CBiteOptMinimize* opt, CBiteRnd* rnd) { return opt->optimize(*rnd); }
+extern "C" void opt_best(CBiteOptMinimize* opt, double* x) { memcpy(x, opt->getBestParams(), opt->N * sizeof(double)); }
 
 // TOOD: expose all parameters
 // TODO: skip steps without position change
-
-extern "C" Minimizer* minimizer_new(int N, biteopt_func f, void* data, double* lb, double* ub, int M, biteopt_rng rf)
-{
-	return new Minimizer(N, f, data, lb, ub, M, rf);
-}
-extern "C" void minimizer_free(Minimizer* minimizer) { delete minimizer; }
-extern "C" int minimizer_step(Minimizer* minimizer) { return minimizer->step(); }
-extern "C" void minimizer_best(Minimizer* minimizer, double* x) { minimizer->best(x); }
 
 extern "C" int biteopt_minimize_wrapper(const int N, biteopt_func f, void* data,
 	const double* lb, const double* ub, double* x, double* minf,
--- source/Numeric/Biteopt.hs
+++ source/Numeric/Biteopt.hs
@@ -11,29 +11,31 @@ import Foreign
 import Foreign.C
 import Foreign.Utilities
 
+type BiteRnd = Ptr Void -> IO CUInt
+foreign import ccall "wrapper" rngWrapper :: Wrapper BiteRnd
+
+data Rnd
+foreign import ccall "rnd_new" rndNew :: IO (Ptr Rnd)
+foreign import ccall "rnd_free" rndFree :: Ptr Rnd -> IO ()
+foreign import ccall "rnd_init" rndInit :: Ptr Rnd -> CInt -> FunPtr BiteRnd -> Ptr Void -> IO ()
+
 type BiteObj = CInt -> Ptr CDouble -> Ptr Void -> IO CDouble
 foreign import ccall "wrapper" objWrapper :: Wrapper BiteObj
 
-type BiteRnd = Ptr Void -> IO CUInt
-foreign import ccall "wrapper" rngWrapper :: Wrapper BiteRnd
+data Min
+foreign import ccall "opt_new" optNew :: IO (Ptr Min)
+foreign import ccall "opt_free" optFree :: Ptr Min -> IO ()
+foreign import ccall "opt_set" optSet :: Ptr Min -> CInt -> FunPtr BiteObj -> Ptr Void -> Ptr CDouble -> Ptr CDouble -> IO ()
+foreign import ccall "opt_dims" optDims :: Ptr Min -> CInt -> CInt -> IO ()
+foreign import ccall "opt_init" optInit :: Ptr Min -> Ptr Rnd -> IO ()
+foreign import ccall "opt_step" optStep :: Ptr Min -> Ptr Rnd -> IO CInt
+foreign import ccall "opt_best" optBest :: Ptr Min -> Ptr CDouble -> IO ()
 
 foreign import ccall "biteopt_minimize_wrapper" biteoptMinimize ::
     CInt -> FunPtr BiteObj -> Ptr Void ->
     Ptr CDouble -> Ptr CDouble -> Ptr CDouble -> Ptr CDouble ->
     CInt -> CInt -> CInt ->
     CInt -> FunPtr BiteRnd -> Ptr Void -> IO CInt
-
-data Min
-foreign import ccall "minimizer_new" minimizerNew :: CInt -> FunPtr BiteObj -> Ptr Void -> Ptr CDouble -> Ptr CDouble -> CInt -> FunPtr BiteRnd -> IO (Ptr Min)
-foreign import ccall "minimizer_free" minimizerFree :: Ptr Min -> IO ()
-foreign import ccall "minimizer_step" minimizerStep :: Ptr Min -> IO Int
-foreign import ccall "minimizer_best" minimizerBest :: Ptr Min -> Ptr CDouble -> IO ()
-
-biteObj :: ([Double] -> Double) -> ContT r IO (FunPtr BiteObj)
-biteObj f = withWrapper objWrapper eval where
-    eval n p = const $ do
-        xs <- peekArray (fromIntegral n) p
-        return $ coerce $ f $ coerce xs
 
 biteRnd :: [Word32] -> ContT r IO (FunPtr BiteRnd)
 biteRnd xs = lift (newIORef xs) >>= withWrapper rngWrapper . next where
@@ -42,25 +44,38 @@ biteRnd xs = lift (newIORef xs) >>= withWrapper rngWrapper . next where
         writeIORef r xs
         return $ coerce x
 
-get :: Int -> Ptr Min -> ContT r IO [Double]
-get n pm = do
-    lift $ minimizerStep pm
+biteObj :: ([Double] -> Double) -> ContT r IO (FunPtr BiteObj)
+biteObj f = withWrapper objWrapper eval where
+    eval n p = const $ do
+        xs <- peekArray (fromIntegral n) p
+        return $ coerce $ f $ coerce xs
+
+get :: Int -> Ptr Min -> Ptr Rnd -> ContT r IO [Double]
+get n pm pr = do
+    lift $ optStep pm pr
     px <- ContT $ allocaArray n
-    lift $ minimizerBest pm px
+    lift $ optBest pm px
     xs <- lift $ peekArray n px
     return $ coerce xs
 
 minimize' :: Maybe [Word32] -> [(Double, Double)] -> ([Double] -> Double) -> IO [[Double]]
 minimize' rng bounds objective = flip runContT return $ do
+    prf <- maybe (return nullFunPtr) biteRnd rng
+    pr <- lift rndNew
+    lift $ rndInit pr 0 prf nullPtr
+    -- TODO: fromIntegral here?
     let dimensions = length bounds
     po <- biteObj objective
     let (boundLower, boundUpper) = unzip $ coerce bounds
     pbl <- ContT $ withArray boundLower
     pbu <- ContT $ withArray boundUpper
-    pr <- maybe (return nullFunPtr) biteRnd rng
-    pm <- lift $ minimizerNew (fromIntegral dimensions) po nullPtr pbl pbu 1 pr
-    result <- replicateM 100 $ get dimensions pm
-    lift $ minimizerFree pm
+    pm <- lift optNew
+    lift $ optSet pm (fromIntegral dimensions) po nullPtr pbl pbu
+    lift $ optDims pm (fromIntegral dimensions) 1
+    lift $ optInit pm pr
+    result <- replicateM 600 $ get dimensions pm pr
+    lift $ optFree pm
+    lift $ rndFree pr
     return result
 
 minimize :: Maybe [Word32] -> [(Double, Double)] -> ([Double] -> Double) -> IO ([Double], Double, CInt)
