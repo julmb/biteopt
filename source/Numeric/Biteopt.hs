fcdb8a8e6ddacb674792553e1b93baa3e06abb5f
Julian Brunner <julian.brunner@gmail.com>
2022-05-01 16:17:24 +0200

    simplify dimensions

--- source/Numeric/Biteopt.hs
+++ source/Numeric/Biteopt.hs
@@ -52,15 +52,14 @@ foreign import ccall "opt_best" optBest :: Ptr Opt -> Ptr CDouble -> IO ()
 
 opt :: ForeignPtr Rnd -> [(Double, Double)] -> ([Double] -> Double) -> IO (ForeignPtr Opt)
 opt pr bounds objective = flip runContT return $ do
-    -- TODO: fromIntegral here?
-    let dimensions = length bounds
+    let dimensions = fromIntegral $ length bounds
     po <- lift $ objWrapper $ obj objective
-    let (boundLower, boundUpper) = unzip $ coerce bounds
+    let (boundLower, boundUpper) = coerce $ unzip bounds
     pbl <- ContT $ withArray boundLower
     pbu <- ContT $ withArray boundUpper
     pm <- lift $ manage optNew optFree $ trace "obj_free" $ freeHaskellFunPtr po
-    lift $ withForeignPtr pm $ \ pm -> optSet pm (fromIntegral dimensions) po nullPtr pbl pbu
-    lift $ withForeignPtr pm $ \ pm -> optDims pm (fromIntegral dimensions) 1
+    lift $ withForeignPtr pm $ \ pm -> optSet pm dimensions po nullPtr pbl pbu
+    lift $ withForeignPtr pm $ \ pm -> optDims pm dimensions 1
     lift $ withForeignPtr pm $ \ pm -> withForeignPtr pr $ \ pr -> optInit pm pr
     return pm
 
@@ -79,4 +78,6 @@ minimize :: Either Int [Word32] -> [(Double, Double)] -> ([Double] -> Double) ->
 minimize gen bounds objective = unsafePerformIO $ do
     pr <- rnd gen
     pm <- opt pr bounds objective
-    repeatIO $ get (length bounds) pm pr
+    x <- best (length bounds) pm
+    xs <- repeatIO $ get (length bounds) pm pr
+    return $ x : xs
