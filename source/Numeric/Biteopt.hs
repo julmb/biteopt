1d39c255559a40b4fc7e126038ba9c26467a8925
Julian Brunner <julian.brunner@gmail.com>
2022-05-01 17:01:31 +0200

    read best directly

--- source/Numeric/Biteopt.hs
+++ source/Numeric/Biteopt.hs
@@ -48,7 +48,7 @@ foreign import ccall "opt_set" optSet :: Ptr Opt -> CInt -> FunPtr Obj -> Ptr Vo
 foreign import ccall "opt_dims" optDims :: Ptr Opt -> CInt -> CInt -> IO ()
 foreign import ccall "opt_init" optInit :: Ptr Opt -> Ptr Rnd -> IO ()
 foreign import ccall "opt_step" optStep :: Ptr Opt -> Ptr Rnd -> IO CInt
-foreign import ccall "opt_best" optBest :: Ptr Opt -> Ptr CDouble -> IO ()
+foreign import ccall "opt_best" optBest :: Ptr Opt -> IO (Ptr CDouble)
 
 opt :: [(Double, Double)] -> ([Double] -> Double) -> IO (ForeignPtr Opt)
 opt bounds objective = flip runContT return $ do
@@ -66,7 +66,7 @@ inita :: ForeignPtr Opt -> ForeignPtr Rnd -> IO ()
 inita pm pr = withForeignPtr pm $ \ pm -> withForeignPtr pr $ \ pr -> optInit pm pr
 
 best :: Int -> ForeignPtr Opt -> IO [Double]
-best n pm = withForeignPtr pm $ \ pm -> coerce $ buffer n (optBest pm)
+best n pm = coerce $ withForeignPtr pm optBest >>= peekArray n
 
 step :: ForeignPtr Opt -> ForeignPtr Rnd -> IO ()
 step pm pr = withForeignPtr pm $ \ pm -> withForeignPtr pr $ \ pr -> void $ optStep pm pr
